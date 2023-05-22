3d33ae2216ea72525762984df9c2bd6d11bf3e49
Dmitry Babanov <babnov1984@mail.ru>
2023-05-22 15:00:09 +0300

    web-app

---
+++ src/main/java/ru/home/aglar/web/HelloServlet.java
@@ -0,0 +1,23 @@
+package ru.home.aglar.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Hello", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.printf("<html><body>");
+        out.printf("Hello World");
+        out.printf("</body></html>");
+        out.close();
+    }
+}
